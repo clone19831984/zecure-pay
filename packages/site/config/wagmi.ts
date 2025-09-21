7bbd1f3f6a44c1e78214e3918ed80b0e6f60d6ff
clone19831984 <phatbm.test02@gmail.com>
2025-09-21 15:49:44 +0000

    Fix wagmi

--- packages/site/config/wagmi.ts
+++ packages/site/config/wagmi.ts
@@ -5,17 +5,7 @@ import { metaMask, injected } from 'wagmi/connectors';
 export const config = createConfig({
   chains: [sepolia],
   connectors: [
-    injected({
-      name: (detected) => {
-        if (detected?.isOkxWallet) return 'OKX Wallet';
-        if (detected?.isKeplr) return 'Keplr';
-        if (detected?.isSubWallet) return 'SubWallet';
-        if (detected?.isTalisman) return 'Talisman';
-        if (detected?.isBackpack) return 'Backpack';
-        if (detected?.isMetaMask) return 'MetaMask';
-        return 'Injected';
-      },
-    }),
+    injected(),
   ],
   transports: {
     [sepolia.id]: http(),
